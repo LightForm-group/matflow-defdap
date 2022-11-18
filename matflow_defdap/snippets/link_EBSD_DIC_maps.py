f0b82749d24ff3bd893184f9e9cf9455e4d144a9
dongchenhu <47746599+dongchenhu@users.noreply.github.com>
2022-11-18 11:26:36 +0000

    find_grain_algorithm
    Added `find_grain_algorithm` for the function `link_EBSD_DIC_maps`

--- matflow_defdap/snippets/link_EBSD_DIC_maps.py
+++ matflow_defdap/snippets/link_EBSD_DIC_maps.py
@@ -2,11 +2,11 @@ from matflow_defdap import main_func
 
 
 @main_func
-def link_EBSD_DIC_maps(EbsdMap, DicMap, dic_homog_points, dic_min_grain_size,
-                       ebsd_homog_points, transform_type):
+def link_EBSD_DIC_maps(ebsd_map, dic_map, dic_homog_points, dic_min_grain_size,
+                       ebsd_homog_points, transform_type, find_grains_algorithm):
 
-    DicMap.homogPoints = dic_homog_points
-    EbsdMap.homogPoints = ebsd_homog_points
-    DicMap.linkEbsdMap(EbsdMap, transformType=transform_type)
+    dic_map.homogPoints = dic_homog_points
+    ebsd_map.homogPoints = ebsd_homog_points
+    dic_map.linkEbsdMap(ebsd_map, transformType=transform_type)
 
-    DicMap.findGrains(minGrainSize=dic_min_grain_size)
+    dic_map.findGrains(minGrainSize=dic_min_grain_size,find_grains_algorithm=find_grains_algorithm)
