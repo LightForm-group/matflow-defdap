d5c49096a052aeb49ec2d7c13780c1acc9df6fd8
aplowman <adam.jp@live.co.uk>
2022-11-24 10:54:31 +0000

    revert direct commits to master

--- matflow_defdap/main.py
+++ matflow_defdap/main.py
@@ -36,7 +36,7 @@ def load_microstructure_EBSD_DIC():
 
 
 @input_mapper(input_file='inputs.hdf5', task='load_microstructure', method='EBSD+DIC')
-def write_param_file(path, DIC, EBSD, transform_type, root_path, scaling_factor, find_grains_algorithm):
+def write_param_file(path, DIC, EBSD, transform_type, root_path, scaling_factor):
     obj = {
         'dic_filename': DIC['filename'],
         'dic_crop': DIC.get('crop', None),
@@ -50,7 +50,6 @@ def write_param_file(path, DIC, EBSD, transform_type, root_path, scaling_factor,
         'transform_type': transform_type,
         'root_path': root_path,
         'scaling_factor': scaling_factor,
-        'find_grain_algorithm': find_grains_algorithm,
     }
     hickle.dump(obj, path)
 
--- matflow_defdap/snippets/link_EBSD_DIC_maps.py
+++ matflow_defdap/snippets/link_EBSD_DIC_maps.py
@@ -3,10 +3,10 @@ from matflow_defdap import main_func
 
 @main_func
 def link_EBSD_DIC_maps(ebsd_map, dic_map, dic_homog_points, dic_min_grain_size,
-                       ebsd_homog_points, transform_type, find_grains_algorithm):
+                       ebsd_homog_points, transform_type):
 
     dic_map.homogPoints = dic_homog_points
     ebsd_map.homogPoints = ebsd_homog_points
     dic_map.linkEbsdMap(ebsd_map, transformType=transform_type)
 
-    dic_map.findGrains(minGrainSize=dic_min_grain_size,find_grains_algorithm=find_grains_algorithm)
+    dic_map.findGrains(minGrainSize=dic_min_grain_size)
