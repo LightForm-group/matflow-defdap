634b77b0977ca2849178311237698995a121c3af
aplowman <adam.jp@live.co.uk>
2022-11-24 11:02:33 +0000

    fix: findGrains argument

--- matflow_defdap/snippets/link_EBSD_DIC_maps.py
+++ matflow_defdap/snippets/link_EBSD_DIC_maps.py
@@ -9,4 +9,4 @@ def link_EBSD_DIC_maps(ebsd_map, dic_map, dic_homog_points, dic_min_grain_size,
     ebsd_map.homogPoints = ebsd_homog_points
     dic_map.linkEbsdMap(ebsd_map, transformType=transform_type)
 
-    dic_map.findGrains(minGrainSize=dic_min_grain_size,find_grains_algorithm=find_grains_algorithm)
+    dic_map.findGrains(minGrainSize=dic_min_grain_size, algorithm=find_grains_algorithm)
